85ea866aa8f914fb4640b968b47dc74fd6dff583
gharper17 <110205117+gharper17@users.noreply.github.com>
2022-08-16 07:23:46 -0500

    Update scriptHelper.js

--- scriptHelper.js
+++ scriptHelper.js
@@ -99,8 +99,8 @@ function pickPlanet(planets) {
     return planets[newPlanet];  
 }
 
-// //module.exports.addDestinationInfo = addDestinationInfo;
-// module.exports.validateInput = validateInput;
-// module.exports.formSubmission = formSubmission;
-// module.exports.pickPlanet = pickPlanet; 
-// module.exports.myFetch = myFetch;
+module.exports.addDestinationInfo = addDestinationInfo;
+module.exports.validateInput = validateInput;
+module.exports.formSubmission = formSubmission;
+module.exports.pickPlanet = pickPlanet; 
+module.exports.myFetch = myFetch;
